33ac687f5edc19d391d3043c4b59640bb192c5f9
Julian2206 <julian.pcc991@gmail.com>
2022-06-15 16:01:58 +0200

    start-mod(1)

---
+++ script.js
@@ -0,0 +1,59 @@
+/* Scrivere un programma che chieda all’utente:
+Il numero di chilometri da percorrere
+Età del passeggero
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
+il prezzo del biglietto è definito in base ai km (0.26 € al km)
+va applicato uno sconto del 15% per i minorenni
+va applicato uno sconto del 35% per gli over 65. */
+
+/* Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), 
+realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console. */
+
+/* Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo. Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+Nota: Se non vi sentite particolarmente creativi, questo screenshot potrebbe essere un’implementazione da seguire per il secondo milestone.
+Potete scegliere di implementare una soluzione completamente diversa oppure simile, ma in ogni caso cercate di farla vostra. */
+
+const distance = parseInt(prompt("insert km:"));
+const age = parseInt(prompt("Insert your age:"));
+
+let message;
+
+let price = distance * 0.26;
+console.log("The ticket price is: " + price);
+
+if (age < 18) {
+  price = price - (price * 15) / 100;
+  message = "20% discount your new price is:" + price.toFixed(2);
+  console.log(message);
+} else if (age > 65) {
+  price = price - (price * 35) / 100;
+  message = "40% discount your new price is:" + price.toFixed(2);
+  console.log(message);
+} else {
+  message = "Your price is:" + price.toFixed(2) + "$";
+  console.log(message);
+}
+
+//document === DOM
+
+const usrDistance = document.querySelector("#usrDistance");
+
+const usrAge = document.getElementById("#ursAge");
+
+const button = document.getElementById("#button");
+
+button.addEventListener("click", function () {
+  //console.log(distance);
+  //console.log(age);
+  console.log(usrDistance.value);
+  console.log(usrAge.value);
+});
+
+/* const button = document.getElementById("#button");
+document.getElementById("#button").onclick = function () {
+  let usrDistance = document.getElementById("#usrDistance").value;
+  console.log("your distance is:", usrDistance);
+
+  let usrAge = document.getElementById("#usrAge").value;
+  console.log("your age is:", usrAge);
+}; */
